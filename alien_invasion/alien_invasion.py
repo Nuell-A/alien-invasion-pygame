c8c2f6d0fd662401fe9c5f53106b9272b3ea9a44
Nuell <alcala.arredondo1@gmail.com>
2022-05-25 05:11:59 +0100

    Added movement to the ship in all directinos and set its limit at the edges of the screen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,8 +4,11 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
-
+# The following allows you to play in fullscreen
+# self.screen = pygame.display.set_mode(
+#     (0,0), pygame.FULLSCREEN)
+# self.screen_width = self.screen.get_rect().width
+# self.screen_height = self.screen.get_rect().height
 class AlienInvasion:
     '''Class to manage overall game assets and behaviors'''
 
@@ -17,10 +20,11 @@ class AlienInvasion:
         # Each asset has its own surface which is just a pygame.display the one 
         #   defined below is the surface for the whole game 
         # Creates screen with custom size and lets us use it for the other methods
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         # Sets name of the window that appears
         pygame.display.set_caption("Forisha sucks pp")
+
+        # Calls ship.py and gives self as an instance
         self.ship = Ship(self)
         
 
@@ -28,13 +32,16 @@ class AlienInvasion:
         '''Creates main loop for game'''
         while True:
             self._check_events()
-            self.update_screen()
+            self._update_screen()
+            self.ship.update()
 
 
-    def update_screen(self):
+    def _update_screen(self):
+        '''Updates game screen'''
         # Changes background color during each pass of the for loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        
 
         # Makes the most up to date screen visible
         pygame.display.flip()
@@ -42,10 +49,42 @@ class AlienInvasion:
 
     # Using a single leading underscore marks the method/vairable/function as a weak private
     def _check_events(self):
-        # Watches keyboard and mouse actions
+        '''Watches keyboard and mouse actions'''
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._keyup_events(event)
+
+    def _keydown_events(self, event):
+        '''Tracks key presses'''
+        if event.key == pygame.K_d:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_a:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_s:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_w:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+
+    def _keyup_events(self, event):
+        '''Tracks key releases'''
+        if event.key == pygame.K_d:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_a:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_s:
+            self.ship.moving_down = False
+        elif event.key == pygame.K_w:
+            self.ship.moving_up = False
+
+            
+                    
             
 
 # Using the if statement below; the game will only run if the alien_invasion.py 
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -6,6 +6,7 @@ class Ship:
         '''Initialize ship and set its starting position'''
         self.screen = ai_game.screen 
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load ship image and get its rect
         self.image = pygame.image.load('alien_invasion/assets/ship.bmp')
@@ -13,6 +14,35 @@ class Ship:
 
         # Start a new ship in the bottom-middle
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Flags for movement
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+        # Storing a decimal value for the ship for finer control
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+    def update(self):
+        '''Decides ships movement based on self.moving_right (True or False)'''
+        # Here we update the ships x value not the rect.
+        #  Also keeps the image from going past the edges. 
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+
+        # Sets ships rect value to the x value (wihout this the rect (ship) wouldn't move)
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
+        
 
 
     def blitme(self):
